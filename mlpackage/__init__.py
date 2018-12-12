b140c5cdc0047457bbc88771fb7f4ac33787b390
kirillsakh <45793864+kirillsakh@users.noreply.github.com>
2018-12-12 19:20:45 +0100

    Create __init__.py

---
+++ mlpackage/__init__.py
@@ -0,0 +1,15 @@
+"""
+Custom machine learning module for Python 
+==================================
+
+mlpack is a Python module integrating selected machine
+learning algorithms in the tightly-knit world of scientific Python
+packages (numpy, scipy, matplotlib).
+
+It aims to provide simple and efficient solutions to learning problems
+that are accessible to everybody and reusable in various contexts:
+machine-learning as a versatile tool for science and engineering.
+"""
+__author__      = "kirill.sakhnov@gmail.com"
+__copyright__   = "Copyright (c) 2018"
+
