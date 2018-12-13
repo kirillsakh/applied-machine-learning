2f2ab46e06ce220b3eb54a39a218f419dbeb95fb
kirillsakh <45793864+kirillsakh@users.noreply.github.com>
2018-12-13 18:00:14 +0100

    Update __init__.py

--- mlpackage/__init__.py
+++ mlpackage/__init__.py
@@ -6,7 +6,7 @@ mlpackage is a Python set of modules integrating selected machine
 learning algorithms in the scientific Python
 packages (numpy, scipy, matplotlib).
 
-It aims to provide simple and efficient solutions to learning problems
+It aims to provide simple environment to learning problems
 that are accessible to everybody and reusable in various contexts:
 machine-learning as a versatile tool for science and engineering.
 """
